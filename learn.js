afa289b87c6e07eef6120907188f43b46b679b37
kep21 <kep135210@gmail.com>
2022-03-30 20:22:14 +0300

    next task

--- learn.js
+++ learn.js
@@ -2,11 +2,11 @@
 
 const num = [1,3,8]
 
-const numFilter = (arr,x=5) => {
+const multiplyBy = (arr,x=5) => {
 	return arr.map(item=> item*x)
 }
 
-console.log(numFilter(num))
+console.log(multiplyBy(num))
 
 
 
@@ -14,10 +14,10 @@ console.log(numFilter(num))
 
 const number = [2,3,5,9,14]
 
-const numberFilter = (arr,x=7) => {
+const filterLessThan = (arr,x) => {
 	return arr.filter(item=> item<=x)
 }
-console.log(numberFilter(number,x=7))
+console.log(filterLessThan(number,7))
 
 
 
@@ -27,11 +27,20 @@ function isEqualTo3 (element) {
  return element === 3
 }
 
-console.log([0,2,3,7].some(isEqualTo3))
-console.log([1,4,8,9].some(isEqualTo3))
+console.log([0,2,3,7].includes(3))
+console.log([1,4,8,9].find(3))
 
 
 // Задача 4
+
+const arr =  [[1,2,5, 'putin'], [3,7,1, 'xuylo']]
+
+const sum = arr => arr.reduce((res, el) => res + (Array.isArray(el) ? sum(el) : el),0);
+
+console.log(sum(arr))
+
+
+
 
 
 
